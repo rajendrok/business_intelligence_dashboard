1787096de550ae4005bfb2d54457849c8834f8de
Rajendro1 <rajandroprosad@gmail.com>
2025-07-13 12:27:13 +0530

    t

--- backend/config/config.go
+++ backend/config/config.go
@@ -3,7 +3,7 @@ package config
 import "context"
 
 var (
-	DRUID_IngestEndpoint   = "https://druid.dev.wikibedtimestories.com/druid/indexer/v1/task"
+	DRUID_IngestEndpoint   = "https://druid.dev.wikibedtimestories.com/druid/indexer/v1"
 	DRUID_SQLQueryEndpoint = "https://druid.dev.wikibedtimestories.com/druid/v2/sql"
 
 	CTX = context.Background()
