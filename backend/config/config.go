4aa5bb95aa6adce104783fb781ad9cedc6391dc2
Rajendro1 <rajandroprosad@gmail.com>
2025-07-13 12:28:00 +0530

    Update DRUID endpoints to use production URLs

--- backend/config/config.go
+++ backend/config/config.go
@@ -3,8 +3,8 @@ package config
 import "context"
 
 var (
-	DRUID_IngestEndpoint   = "https://druid.dev.wikibedtimestories.com/druid/indexer/v1"
-	DRUID_SQLQueryEndpoint = "https://druid.dev.wikibedtimestories.com/druid/v2/sql"
+	DRUID_IngestEndpoint   = "https://druid62.wikibedtimestories.com/druid/indexer/v1"
+	DRUID_SQLQueryEndpoint = "https://druid62.wikibedtimestories.com/druid/v2/sql"
 
 	CTX = context.Background()
 )
